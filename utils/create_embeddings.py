5d8c90948f0c659349c2d4fba703634ffa7f1ab7
Aryan kakran <145355541+akakran22@users.noreply.github.com>
2025-09-13 11:32:32 +0530

    Update create_embeddings.py

--- utils/create_embeddings.py
+++ utils/create_embeddings.py
@@ -1,21 +1,3 @@
-# from sentence_transformers import SentenceTransformer
-# import numpy as np
-
-# class Embeddings:
-#     def __init__(self, model_name="sentence-transformers/all-mpnet-base-v2"):
-#         self.model = SentenceTransformer(model_name)
-
-#     def encode(self, texts, batch_size=64, normalize=True):
-#         vecs = self.model.encode(
-#             texts,
-#             batch_size=batch_size,
-#             show_progress_bar=True,
-#             normalize_embeddings=normalize
-#         )
-#         return vecs.astype(np.float32)
-
-
-# utils/create_embeddings.py
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import os
@@ -80,3 +62,4 @@ class Embeddings:
         }
         self.save_embeddings(vector_dir, vectors, meta, cache_name)
         return vectors
+
